3e45f7ce3f73f515074aa4d8c6fd8363c90c5b7f
ALAN EVANDRO BARBOZA <35473833+alanebarboza@users.noreply.github.com>
2021-10-24 12:04:24 -0300

    Update 20211020173600_First_CreateDataBase.Designer.cs

--- UserNotification.Infra/Migrations/20211020173600_First_CreateDataBase.Designer.cs
+++ UserNotification.Infra/Migrations/20211020173600_First_CreateDataBase.Designer.cs
@@ -46,7 +46,7 @@ namespace UserNotification.Infra.Migrations
                         .HasColumnType("nvarchar(20)");
 
                     b.Property<string>("PassWord")
-                        .HasColumnType("nvarchar(max)");
+                        .HasColumnType("nvarchar(20)");
 
                     b.Property<string>("Phone")
                         .HasMaxLength(15)
