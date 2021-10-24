1a6e1866e2d8f138950822694cd56cb8e9585d4d
ALAN EVANDRO BARBOZA <35473833+alanebarboza@users.noreply.github.com>
2021-10-24 12:15:16 -0300

    Update UsersEntityTypeConfiguration.cs

--- UserNotification.Infra/EntityTypeConfiguration/UsersEntityTypeConfiguration.cs
+++ UserNotification.Infra/EntityTypeConfiguration/UsersEntityTypeConfiguration.cs
@@ -11,6 +11,7 @@ namespace UserNotification.Infra.EntityTypeConfiguration
             builder.HasKey(c => c.Id);
             builder.Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
             builder.Property(c => c.Nick).HasMaxLength(20).IsRequired();
+            builder.Property(c => c.PassWord).HasMaxLength(20).IsRequired();
             builder.Property(c => c.Name).HasMaxLength(80).IsRequired();
             builder.Property(c => c.Phone).HasMaxLength(15);
             builder.Property(c => c.Email).HasMaxLength(50);
